1771de305f3a0094e7a646387800e4b650c7a026
tafarouk <tafarouk@me.com>
2016-02-27 17:09:18 +0400

    Update RankWords.py

--- RankWords.py
+++ RankWords.py
@@ -1,8 +1,14 @@
 import nltk
+from nltk.util import ngrams
 def RankWords(filename):
     document = open(filename).read()
     words=nltk.tokenize.word_tokenize(document)
-    print (words)
+    g.add_nodes_from(words)
+    bg=ngrams(words,2)
+    g.add_edges_from(bg)
+    
+    g.nodes()
+    g.edges()
     
     
 if __name__ == '__main__':
