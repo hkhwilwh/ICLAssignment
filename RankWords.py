524b2a9f428da5f3289c49e17d2b06bef2a0b98d
hkhwilwh <hassan.khwileh@gmail.com>
2016-02-27 16:55:17 +0400

    Update RankWords.py

--- RankWords.py
+++ RankWords.py
@@ -1,10 +1,9 @@
 import nltk
 def RankWords(filename):
-	document = open(filename).read()
-	words=nltk.tokenize.word_tokenize(document)
-	Print (words)
-
-def main():
+    document = open(filename).read()
+    words=nltk.tokenize.word_tokenize(document)
+    print (words)
+    
+    
+if __name__ == '__main__':
     RankWords("Test.txt")
-
-    main()
